8ff4f68fa859b6129e97b26ae784a54cb616b1a0
Matthew Dunlap <TriNitroToluene5@gmail.com>
2020-02-22 00:10:25 -0500

    Added parameters to the Welcome method.

--- MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -13,12 +13,12 @@ namespace MvcMovie.Controllers
             return "This is my default action...";
         }
 
-        // 
         // GET: /HelloWorld/Welcome/ 
+        // Requires using System.Text.Encodings.Web;
 
-        public string Welcome()
+        public string Welcome(string name, int numTimes = 1)
         {
-            return "This is the Welcome action method...";
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
         }
     }
 }
